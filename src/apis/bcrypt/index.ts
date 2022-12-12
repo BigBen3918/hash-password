5fd382b7750f4e8f4a9d4ce81e89278fded5591f
bigben3918 <benjamin199551@gmail.com>
2022-12-13 01:18:05 +0800

    add: bcrypt hash

---
+++ src/apis/bcrypt/index.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import setlog from "../../utils/setlog";
+
+const bcrypt_Hash = async (req: Request, res: Response) => {
+    try {
+        const { password } = req.body;
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        console.log("result: ", hashedPassword);
+
+        res.json({
+            hashedPass: hashedPassword,
+        });
+    } catch (err: any) {
+        setlog("bcrypt issue:", err.message);
+    }
+};
+
+const compare_bcrypt_Hash = async (req: Request, res: Response) => {
+    try {
+        const { hash, password } = req.body;
+        let isMatch = await bcrypt.compare(password, hash);
+        if (isMatch) {
+            res.json({
+                result: "password matched",
+            });
+        } else {
+            res.json({
+                result: "password dismatched",
+            });
+        }
+    } catch (err: any) {
+        setlog("bcrypt compare issue: ", err.message);
+    }
+};
+
+export { bcrypt_Hash, compare_bcrypt_Hash };
--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,12 @@
-import { argon2_Hash, compare_argon2_Hash } from "./argon2/argon2";
+import { argon2_Hash, compare_argon2_Hash } from "./argon2";
+import { bcrypt_Hash, compare_bcrypt_Hash } from "./bcrypt";
 
 const RestApi = (router: any) => {
-    // APIs
+    // Hash APIs
     router.post("/argon2", argon2_Hash);
     router.post("/argon2-compare", compare_argon2_Hash);
+    router.post("/bcrypt", bcrypt_Hash);
+    router.post("/bcrypt-compare", compare_bcrypt_Hash);
 };
 
 export default RestApi;
